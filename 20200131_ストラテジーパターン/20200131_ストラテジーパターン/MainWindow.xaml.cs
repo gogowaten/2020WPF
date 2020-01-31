c8c31376b5caae7cdd2b5f2aab2ae01316ffbe8b
waten <gogowaten@yahoo.co.jp>
2020-01-31 15:51:28 +0900

    202001311551

--- 20200131_ストラテジーパターン/20200131_ストラテジーパターン/MainWindow.xaml.cs
+++ 20200131_ストラテジーパターン/20200131_ストラテジーパターン/MainWindow.xaml.cs
@@ -26,16 +26,75 @@ namespace _20200131_ストラテジーパターン
         {
             InitializeComponent();
 
+            //抽象クラス版
             var neko = new CCC(new BBB());
-            neko.Select();
+            neko.Execute();
+            neko = new CCC(new AAA());
+            neko.Execute();
 
-            var inu = new CCC(new AAA());
-            inu.Select();
+            //インターフェース版
+            var uma = new CCCI(new AAAI());
+            uma.Execute();
+            uma = new CCCI(new BBBI());
+            uma.Execute();
+
+            var inu = new LLL(new Selecter1(), new Splitter1());
+            inu.Execute();
+            inu = new LLL(new Selecter2(), new Splitter1());
+            inu.Execute();
 
         }
     }
 
 
+    public interface ISelect
+    {
+        void Select();
+    }
+    public interface ISplit
+    {
+        void Split();
+    }
+    public class Selecter1 : ISelect
+    {
+        public void Select() => MessageBox.Show(nameof(Selecter1));
+    }
+    public class Selecter2 : ISelect
+    {
+        public void Select() => MessageBox.Show(nameof(Selecter2));
+    }
+    public class Splitter1 : ISplit
+    {
+        public void Split() => MessageBox.Show(nameof(Splitter1));
+    }
+    public class Splitter2 : ISplit
+    {
+        public void Split() => MessageBox.Show(nameof(Splitter2));
+    }
+
+
+    public class LLL
+    {
+        private ISelect Ee;//ここはインターフェースでもいいみたい、これで選択できる
+        private ISplit Ff;
+        public LLL(ISelect e1, ISplit f1)
+        {
+            Ee = e1;
+            Ff = f1;
+        }
+
+        public void Execute()
+        {
+            Ee.Select();
+            Ff.Split();
+        }
+    }
+
+
+
+
+
+    #region 抽象クラス版
     public abstract class ZZZ
     {
         public abstract void Select();
@@ -52,35 +111,32 @@ namespace _20200131_ストラテジーパターン
     {
         private ZZZ Z;
         public CCC(ZZZ z) => this.Z = z;
-        public void Select() => Z.Select();
+        public void Execute() => Z.Select();
     }
+    #endregion
 
 
-
-    #region 失敗
-    //public interface IZZZ
-    //{
-    //    void Select();
-    //    void Split();
-    //}
-    //public class AAA : IZZZ
-    //{
-    //    public void Select() => MessageBox.Show("AAA");
-    //    void IZZZ.Split() => MessageBox.Show("AAA");
-    //}
-    //public class BBB : IZZZ
-    //{
-    //    public void Split() => MessageBox.Show("BBB");
-    //    void IZZZ.Select() => MessageBox.Show("BBB");
-    //}
-    //public class CCC
-    //{
-    //    private IZZZ ZZZ;
-
-    //    public void Execute()
-    //    {
-    //        ZZZ.Select();
-    //    }
-    //}
+    #region インターフェース版
+    public interface IZZZ
+    {
+        void Select();
+        void Split();
+    }
+    public class AAAI : IZZZ
+    {
+        public void Select() => MessageBox.Show("AAA");
+        void IZZZ.Split() => MessageBox.Show("AAA");
+    }
+    public class BBBI : IZZZ
+    {
+        public void Split() => MessageBox.Show("BBB");
+        void IZZZ.Select() => MessageBox.Show("BBB");
+    }
+    public class CCCI
+    {
+        private IZZZ ZZZ;
+        public CCCI(IZZZ iz) => ZZZ = iz;
+        public void Execute() => ZZZ.Select();
+    }
     #endregion
 }
