6f1dcd6791105b3c78f9620a2376b9415cf12a33
waten <gogowaten@yahoo.co.jp>
2020-03-09 20:55:31 +0900

    2020030920055

--- 20200131_ストラテジーパターン/20200131_ストラテジーパターン/MainWindow.xaml.cs
+++ 20200131_ストラテジーパターン/20200131_ストラテジーパターン/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection;
+
+
 //【Unity】Unityで学ぶStrategyパターン！モンスターに色んな技を使わせよう！【C#】【プログラム設計】 | サプライドの技術者BLOG
 //https://techlife.supride.jp/archives/1411
 
@@ -27,49 +30,54 @@ namespace _20200131_ストラテジーパターン
             InitializeComponent();
 
             //抽象クラス版
-            var neko = new CCC(new BBB());
+            AbstTestClass neko;
+            neko = new AbstTestClass(new AbsClsSelecter1(), new AbsClsSplitter1());
+            //neko.Select();
+            //neko.Split();
             neko.Execute();
-            neko = new CCC(new AAA());
+            neko = new AbstTestClass(new AbsClsSelecter2(), new AbsClsSplitter2());
+            //neko.Select();
+            //neko.Split();
             neko.Execute();
 
             //インターフェース版
-            var uma = new CCCI(new AAAI());
-            uma.Execute();
-            uma = new CCCI(new BBBI());
-            uma.Execute();
-
             var inu = new LLL(new Selecter1(), new Splitter1());
             inu.Execute();
             inu = new LLL(new Selecter2(), new Splitter1());
             inu.Execute();
 
+            //CCCI uma;
+            //uma = new CCCI(new AAAI());
+            //uma.Execute();
+            //uma = new CCCI(new BBBI());
+            //uma.Execute();
         }
     }
 
-
+    #region インターフェース版
     public interface ISelect
     {
         void Select();
+    }
+    public class Selecter1 : ISelect
+    {
+        public void Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    }
+    public class Selecter2 : ISelect
+    {
+        public void Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
     }
     public interface ISplit
     {
         void Split();
     }
-    public class Selecter1 : ISelect
-    {
-        public void Select() => MessageBox.Show(nameof(Selecter1));
-    }
-    public class Selecter2 : ISelect
-    {
-        public void Select() => MessageBox.Show(nameof(Selecter2));
-    }
     public class Splitter1 : ISplit
     {
-        public void Split() => MessageBox.Show(nameof(Splitter1));
+        public void Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
     }
     public class Splitter2 : ISplit
     {
-        public void Split() => MessageBox.Show(nameof(Splitter2));
+        public void Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
     }
 
 
@@ -89,54 +97,80 @@ namespace _20200131_ストラテジーパターン
             Ff.Split();
         }
     }
-
-
+    #endregion
 
 
 
     #region 抽象クラス版
-    public abstract class ZZZ
+    public abstract class AbstSelectBase
     {
         public abstract void Select();
     }
-    public class AAA : ZZZ
+    public class AbsClsSelecter1 : AbstSelectBase
     {
-        public override void Select() => MessageBox.Show("AAA");
+        public override void Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
     }
-    public class BBB : ZZZ
+    public class AbsClsSelecter2 : AbstSelectBase
     {
-        public override void Select() => MessageBox.Show("BBB");
+        public override void Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
     }
-    public class CCC
+
+    public abstract class AbstSplitBase
     {
-        private ZZZ Z;
-        public CCC(ZZZ z) => this.Z = z;
-        public void Execute() => Z.Select();
+        public abstract void Split();
+    }
+    public class AbsClsSplitter1 : AbstSplitBase
+    {
+        public override void Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    }
+    public class AbsClsSplitter2 : AbstSplitBase
+    {
+        public override void Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    }
+
+    public class AbstTestClass
+    {
+        private AbstSelectBase SelectBase;
+        private AbstSplitBase SplitBase;
+        public AbstTestClass(AbstSelectBase selectB, AbstSplitBase splitB)
+        {
+            this.SelectBase = selectB;
+            this.SplitBase = splitB;
+        }
+
+        public void Select() => SelectBase.Select();
+        public void Split() => SplitBase.Split();
+        public void Execute() { SelectBase.Select(); SplitBase.Split(); }
     }
     #endregion
 
 
     #region インターフェース版
-    public interface IZZZ
-    {
-        void Select();
-        void Split();
-    }
-    public class AAAI : IZZZ
-    {
-        public void Select() => MessageBox.Show("AAA");
-        void IZZZ.Split() => MessageBox.Show("AAA");
-    }
-    public class BBBI : IZZZ
-    {
-        public void Split() => MessageBox.Show("BBB");
-        void IZZZ.Select() => MessageBox.Show("BBB");
-    }
-    public class CCCI
-    {
-        private IZZZ ZZZ;
-        public CCCI(IZZZ iz) => ZZZ = iz;
-        public void Execute() => ZZZ.Select();
-    }
+    ////これは大変、組み合わせの分だけクラスを書く必要がある
+    //public interface IZZZ
+    //{
+    //    void Select();
+    //    void Split();
+    //}
+    //public class AAAI : IZZZ
+    //{
+    //    public void Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    //    void IZZZ.Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    //}
+    //public class BBBI : IZZZ
+    //{
+    //    public void Split() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    //    void IZZZ.Select() => MessageBox.Show($"{ToString()}の{MethodBase.GetCurrentMethod()}");
+    //}
+    //public class CCCI
+    //{
+    //    private IZZZ ZZZ;
+    //    public CCCI(IZZZ iz) => ZZZ = iz;
+    //    public void Execute()
+    //    {
+    //        ZZZ.Select();
+    //        ZZZ.Split();
+    //    }
+    //}
     #endregion
 }
---
+++ 20200308_ストラテジーパターン/20200308_ストラテジーパターン/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+//C#で学ぶデザインパターン入門 ⑥Prototype - Qiita
+//https://qiita.com/toshi0607/items/f4358020befca048d2e0
+
+//Factory Methodに近いけど、これだと大げさかな、ちょっと違う
+//Prototype、これのが近いかな
+//Builderは違う
+//Abstract Factory、近い気もするけど難しすぎてわからん
+//Bridge、いい気がするけど少し違うかも、いやかなり近いかも、namespace使わないのがいい
+
+
+namespace _20200308_ストラテジーパターン
+{
+    using Framework;
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //var neko = Encoding.GetEncodings();
+            //foreach (var item in neko)
+            //{
+            //    Debug.WriteLine(item.DisplayName);
+            //    Debug.WriteLine(item.Name);
+            //}
+
+            Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
+            Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
+            CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+            d1.Show();
+            d2.Show();
+            d3.Show();
+            d3.MultiDisplay(2);
+            var dd = new CountDisplay(new StringDisplayImpl("yukkuri"));
+            
+        }
+    }
+
+
+    public class Display
+    {
+        private DisplayImpl impl;
+        public Display(DisplayImpl impl)
+        {
+            this.impl = impl;
+        }
+
+        public void Open()
+        {
+            impl.RawOpen();
+        }
+
+        public void Print()
+        {
+            impl.RawPrint();
+        }
+
+        public void Close()
+        {
+            impl.RawClose();
+        }
+
+        public void Show()
+        {
+            Open();
+            Print();
+            Close();
+        }
+    }
+
+    // RefinedAbstraction
+    // ・Abstractionに対して機能を追加
+    public class CountDisplay : Display
+    {
+        public CountDisplay(DisplayImpl impl) : base(impl) { }
+
+        public void MultiDisplay(int times)
+        {
+            Open();
+            for (int i = 0; i < times; i++)
+            {
+                Print();
+            }
+            Close();
+        }
+    }
+
+    // Implementor
+    // ・実装のクラス階層の最上位クラス
+    // ・Abstranctionのインターフェース（API）を規定する
+    public abstract class DisplayImpl
+    {
+        public abstract void RawOpen();
+        public abstract void RawPrint();
+        public abstract void RawClose();
+    }
+
+    // ConcreteImplementator
+    // ・Implementatorを具体的に実装する
+    public class StringDisplayImpl : DisplayImpl
+    {
+        private string str;
+        private int width;
+        public StringDisplayImpl(string str)
+        {
+            this.str = str;
+            Encoding sjisEnc = Encoding.GetEncoding("shift_jis");
+            this.width = sjisEnc.GetByteCount(str);
+        }
+
+        public override void RawOpen()
+        {
+            PrintLine();
+        }
+
+        public override void RawPrint()
+        {
+            Console.WriteLine($"|{str}|");
+        }
+
+        public override void RawClose()
+        {
+            PrintLine();
+        }
+
+        public void PrintLine()
+        {
+            Console.Write("+");
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
+
+
+namespace Framework
+{
+    // Prototype
+    public interface Product : ICloneable
+    {
+        void Use(string s);
+        Product CreateClone();
+    }
+
+    // Client
+    public class Manager
+    {
+        private Dictionary<string, Product> showcase = new Dictionary<string, Product>();
+        public void Register(string name, Product proto)
+        {
+            showcase.Add(name, proto);
+        }
+        public Product Create(string protoname)
+        {
+            Product p = showcase[protoname];
+            return p.CreateClone();
+        }
+    }
+}
+
+
+
+
+
