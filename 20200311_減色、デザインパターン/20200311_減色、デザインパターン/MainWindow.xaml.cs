18cc2cf2501d8ebd8dc7370462054d585534f9bc
waten <gogowaten@yahoo.co.jp>
2020-03-14 08:22:04 +0900

    202003140821

--- 20200311_減色、デザインパターン/20200311_減色、デザインパターン/MainWindow.xaml.cs
+++ 20200311_減色、デザインパターン/20200311_減色、デザインパターン/MainWindow.xaml.cs
@@ -40,12 +40,14 @@ namespace _20200311_減色_デザインパターン
 
             var cube = new Cube1(MyPixels);
             cube.SplitCube(3, cube);
-            
+
 
         }
     }
 
-    #region
+    //選択と分割の種類が増えると管理が大変だし、計算に使うプロパティの初期化も関係ないものまで全部計算するか、
+    //必要なのものだけその都度計算するかになって、どちらにしても無駄が多くなる
+    #region 一括クラス、
     public class Cube1
     {
         public byte[] Values;
@@ -108,6 +110,148 @@ namespace _20200311_減色_デザインパターン
 
 
     #endregion
+    //__________________________________________
+    //組み合わせが増えても問題ないけど、初期化ができないのでその都度計算になって無駄がかなり多くなる
+    #region 選択と分割を別クラス。
+
+    public interface ISelecter
+    {
+        Cube2 Select(List<Cube2> cubes);
+    }
+    public class SelecterA : ISelecter
+    {
+        public Cube2 Select(List<Cube2> cubes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ISplitter2
+    {
+        (Cube2 cubeA, Cube2 cubeB) Split(Cube2 cube);
+    }
+    public class SplitterA : ISplitter2
+    {
+        public (Cube2 cubeA, Cube2 cubeB) Split(Cube2 cube)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Cube2
+    {
+        private byte[] Pixels;
+        private ISelecter Select;
+        private ISplitter2 Splitter;
+        public List<Cube2> ListCubes = new List<Cube2>();
+        public Cube2(byte[] pixesl, ISelecter selecter, ISplitter2 splitter)
+        {
+            Pixels = pixesl;
+            ListCubes.Add(this);
+            Select = selecter;
+            Splitter = splitter;
+        }
+
+        public void Split(int count)
+        {
+            while (ListCubes.Count < count)
+            {
+                Splitter.Split(Select.Select(this.ListCubes));
+            }
+        }
+    }
+
+    #endregion
+
+    #region
+    //必要なプロパティ群をインターフェースを付加したクラスに置いてみたけど、うまく行かない
+    public interface ISelecter3
+    {
+        Cube3 Select(List<Cube3> cubes);
+    }
+    public class SelecterA3 : ISelecter3
+    {
+        private byte Min;
+        private byte Max;
+        public SelecterA3(byte[] pixels)
+        {
+            //ここでminmaxを計算
+        }
+        public Cube3 Select(List<Cube3> cubes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ISplitter3
+    {
+        (Cube3 cubeA, Cube3 cubeB) Split(Cube3 cube);
+    }
+    public class SplitterA3 : ISplitter3
+    {
+        ISelecter3 Selecter;
+        public SplitterA3(ISelecter3 selecter)
+        {
+            Selecter = selecter;
+        }
+        public (Cube3 cubeA, Cube3 cubeB) Split(Cube3 cube)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Cube3
+    {
+        private List<Cube3> CubeList = new List<Cube3>();
+        private byte[] Pixels;
+        private ISelecter3 Selecter;
+        private ISplitter3 Splitter;
+        public Cube3(byte[] pixels, ISelecter3 selecter, ISplitter3 splitter)
+        {
+            Pixels = pixels;
+            Selecter = selecter;
+            Splitter = splitter;
+            CubeList.Add(this);
+        }
+        public void Exe(int count)
+        {
+            Splitter.Split(Selecter.Select(CubeList));
+        }
+    }
+
+    #endregion
+
+    #region
+    public abstract class SelecterBase
+    {
+        public byte Min;
+        public byte Max;
+    }
+    public class Selecter4 : SelecterBase
+    {
+        byte[] Pixels;
+        public Selecter4(byte[] pixels)
+        {
+            Pixels = pixels;
+            Min = 0;
+            Max = 255;
+        }
+    }
+    public interface ISplitter4
+    {
+        public (Cube4 cubeA, Cube4 cubeB) Split(Cube4 cube);
+    }
+    public class SplitterA4 : ISplitter4
+    {
+        public (Cube4 cubeA, Cube4 cubeB) Split(Cube4 cube)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Cube4
+    {
+
+    }
+    #endregion
+
 
 
 
@@ -170,33 +314,6 @@ namespace _20200311_減色_デザインパターン
             AbSelect.Select();
         }
     }
-
-    #region インターフェースだとプロパティをもたせられない
-    public interface ISelect
-    {
-        void Select();
-    }
-    public class SelectA : ISelect
-    {
-        public void Select() => Debug.WriteLine($"{GetType().Name}  {System.Reflection.MethodBase.GetCurrentMethod().Name}");
-    }
-    public class SelectB : ISelect
-    {
-        public void Select() => Debug.WriteLine($"{System.Reflection.MethodBase.GetCurrentMethod().Name}");
-    }
-    public class MySelect
-    {
-        public ISelect Selecter { get; set; }
-        public MySelect(ISelect select)
-        {
-            Selecter = select;
-        }
-        public void Exe()
-        {
-            Selecter.Select();
-        }
-    }
-    #endregion
 
 
 
