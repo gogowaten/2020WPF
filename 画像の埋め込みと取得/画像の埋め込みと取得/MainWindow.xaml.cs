f53e333d567d359b89c0969bfb573e03e06db4a8
waten <gogowaten@yahoo.co.jp>
2020-04-02 22:14:40 +0900

    202004022214

---
+++ 画像の埋め込みと取得/画像の埋め込みと取得/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace 画像の埋め込みと取得
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "画像の埋め込みと取得.grayScale.bmp";
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(path))
+            {
+                if (stream != null)
+                {
+                    MyImage.Source = BitmapFrame.Create(stream);
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "画像の埋め込みと取得.Myフォルダー.HSVRectHue000.png";
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(path))
+            {
+                if (stream != null)
+                {
+                    MyImage.Source = BitmapFrame.Create(stream);
+                }
+            }
+        }
+
+        //未使用
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {  
+            var bmp = Properties.Resources.HSVRectH90;
+            MyImage.Source = BitmapSource.Create(
+                256, 256, 96, 96, PixelFormats.Bgra32, null, bmp, 256 * 4);
+        }
+    }
+}
