1fd30b82653c68e10ed517bcdbc0e0098f851273
waten <gogowaten@yahoo.co.jp>
2020-04-10 11:21:42 +0900

    202004101121

--- 20200410_正規分布の面積、累積分布/20200410_正規分布の面積、累積分布/MainWindow.xaml.cs
+++ 20200410_正規分布の面積、累積分布/20200410_正規分布の面積、累積分布/MainWindow.xaml.cs
@@ -13,17 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-//エクセルのNORMDIST関数の最後の引数にtrueを指定すると、累積分布関数になる
-//これはグラフだと面積、全体に対数割合になるみたい、これをC#で真似てみる
-//確率密度関数
-//累積分布関数
-
-
-
-//    確率累積密度関数の実装 - カテキン魂
-//https://blog.goo.ne.jp/javasoft/e/3f4245d8b580b9977529420cc80bd02e
-
-namespace _20200409_正規分布_累積分布関数
+namespace _20200410_正規分布の面積_累積分布
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -33,12 +23,7 @@ namespace _20200409_正規分布_累積分布関数
         public MainWindow()
         {
             InitializeComponent();
-
-            Test();
-            int[] vs = new int[] { 3, 4, 0, 0, 0, 1, 1, 4, 0, 1, 2, 2, 3, 0, 3, 3, 0, 0, 4, 4 };
-            var result = MyCalc(vs, 1m, 2m, 100m);
         }
-
         private void Test()
         {
             int[] vs = new int[] { 3, 4, 0, 0, 0, 1, 1, 4, 0, 1, 2, 2, 3, 0, 3, 3, 0, 0, 4, 4 };
@@ -54,25 +39,10 @@ namespace _20200409_正規分布_累積分布関数
 
             decimal begin2 = 1.0m;
             decimal end2 = 2.0m;
-            var vv = CumulativeDistribution(begin2, end2, average, varp, 100m);
+            var vv = Menseki(begin2, end2, average, varp, 100m);
         }
 
-        private decimal MyCalc(int[] specimen, decimal begin, decimal end,decimal resolution)
-        {
-            double average = specimen.Average();
-            var variance = Variance(specimen, average);            
-            return CumulativeDistribution(begin, end, average, variance, resolution);
-        }
-        /// <summary>
-        /// 累積分布関数
-        /// </summary>
-        /// <param name="begin">区間開始</param>
-        /// <param name="end">区間終了</param>
-        /// <param name="average">平均値</param>
-        /// <param name="varp">分散(標準偏差^2)</param>
-        /// <param name="resolution">計算精度、区間の分割数で1000あれば十分</param>
-        /// <returns></returns>
-        private decimal CumulativeDistribution(decimal begin, decimal end, double average, double varp, decimal resolution)
+        private decimal Menseki(decimal begin, decimal end, double average, double varp, decimal resolution)
         {
             decimal unit = (end - begin) / resolution;
             decimal total = 0m;
@@ -83,22 +53,15 @@ namespace _20200409_正規分布_累積分布関数
             }
             return total;
         }
-        /// <summary>
-        /// 2点の確率密度関数の距離
-        /// </summary>
-        /// <param name="x1">点1</param>
-        /// <param name="x2">点2</param>
-        /// <param name="average">平均値</param>
-        /// <param name="variance">分散</param>
-        /// <returns></returns>
+
         private decimal NormDist2(decimal x1, decimal x2, decimal average, double variance)
         {
-            double expLeft = 1 / Math.Sqrt(2 * Math.PI * variance);
-            decimal index1 = -((x1 - average) * (x1 - average) / (2 * (decimal)variance));
-            double exp1 = Math.Pow(Math.Exp(1), (double)index1);
-            decimal index2 = -((x2 - average) * (x2 - average) / (2 * (decimal)variance));
-            double exp2 = Math.Pow(Math.Exp(1), (double)index2);
-            return (decimal)(expLeft * exp1) + (decimal)(expLeft * exp2);
+            double d = 1 / Math.Sqrt(2 * Math.PI * variance);
+            decimal er1 = -((x1 - average) * (x1 - average) / (2 * (decimal)variance));
+            double e1 = Math.Pow(Math.Exp(1), (double)er1);
+            decimal er2 = -((x2 - average) * (x2 - average) / (2 * (decimal)variance));
+            double e2 = Math.Pow(Math.Exp(1), (double)er2);
+            return (decimal)(d * e1) + (decimal)(d * e2);
         }
 
         /// <summary>
@@ -128,5 +91,4 @@ namespace _20200409_正規分布_累積分布関数
             return ((double)total / vs.Length) - (average * average);
         }
     }
-
 }
