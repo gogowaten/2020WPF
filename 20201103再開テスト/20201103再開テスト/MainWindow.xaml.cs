aa67b4737d76b30ddee820e17bfc30e029d08be3
waten <gogowaten@yahoo.co.jp>
2020-11-04 12:26:39 +0900

    202011041226

--- 20201103再開テスト/20201103再開テスト/MainWindow.xaml.cs
+++ 20201103再開テスト/20201103再開テスト/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace _20201103再開テスト
 {
     /// <summary>
@@ -21,15 +22,108 @@ namespace _20201103再開テスト
     public partial class MainWindow : Window
     {
         private List<string> MyDirList = new List<string>();
+        private AppData MyAppData = new AppData();
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
 
+            MyTabControl.SelectedIndex = 1;
+            //Test1();
+            Test2();
+
+
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {            
+            
+        }
+
+        private void Test2()
+        {
+            MyAppData.DirList.Add("dir1");
+            MyAppData.DirList.Add("dir2");
+            DataContext = MyAppData;
+            MyAppData.DirList.Add("dir3");
+
+            MyAppData.JpegQuality = 96;
+            MyAppData.WindowTop = 500;
+            MyAppData.WindowLeft = 500;
+
+            MyAppData.WindowWidth = 450;
+            MyAppData.WindowHeight = 350;
+            var b = new Binding();
+            b.Mode = BindingMode.TwoWay;
+            b.Path = new PropertyPath("WindowWidth");
+            BindingOperations.SetBinding(this, WidthProperty, b);
+            b = new Binding();
+            b.Mode = BindingMode.TwoWay;
+            b.Path = new PropertyPath("WindowHeight");
+            BindingOperations.SetBinding(this, HeightProperty, b);
+
+            //MyAppData.WindowWidth = this.Width;
+            //          MyAppData.WindowHeight = 350;
+            
+        }
+        private void Test1()
+        {
             MyDirList.Add(@"C:\Users\waten\Documents");
             MyDirList.Add(@"D:\ブログ用");
 
-            ComboBoxSaveDir.ItemsSource = MyDirList;
+            //ComboBoxSaveDir.ItemsSource = MyDirList;
+            DataContext = MyDirList;
+            MyDirList.Add("test");
 
         }
+
+        private void ButtenTest_Click(object sender, RoutedEventArgs e)
+        {
+            //MyDirList.Add("add2");
+            MyAppData.DirList.Add("dir4");
+            
+            ComboBoxSaveDir.Items.Refresh();
+
+        }
+
+        private void ButtenTest2_Click(object sender, RoutedEventArgs e)
+        {
+            SaveConfig();
+        }
+        private bool SaveConfig()
+        {
+            string savePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + "text.xml";
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(AppData));
+            try
+            {
+                using (var stream = new System.IO.StreamWriter(savePath, false, new System.Text.UTF8Encoding(false)))
+                {
+                    serializer.Serialize(stream, MyAppData);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"保存できなかった\n{ex.Message}");
+                return false;
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class AppData
+    {
+        public AppData()
+        {
+            DirList = new List<string>();
+        }
+        public List<string> DirList { get; set; }
+
+        public int JpegQuality { get; set; }
+        public double WindowTop { get; set; }
+        public double WindowLeft { get; set; }
+        public double WindowWidth { get; set; }
+        public double WindowHeight { get; set; }
     }
 }
