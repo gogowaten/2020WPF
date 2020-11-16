ad15ade057d64de363faac3899ece03b9e81ffc5
waten <gogowaten@yahoo.co.jp>
2020-11-16 23:21:43 +0900

    202011162321

---
+++ 20201116_ウィンドウキャプチャ時の見た目とのズレを修正/20201116_ウィンドウキャプチャ時の見た目とのズレを修正/MainWindow.xaml.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;//Imagingで使っている
+using System.Windows.Interop;//CreateBitmapSourceFromHBitmapで使っている
+using System.Windows.Threading;//DispatcherTimerで使っている
+
+namespace _20201116_ウィンドウキャプチャ時の見た目とのズレを修正
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region WindowsAPI^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        //キーの入力取得
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        //Rect取得用
+        private struct RECT
+        {
+            //型はlongじゃなくてintが正解！！！！！！！！！！！！！！
+            //longだとおかしな値になる
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public override string ToString()
+            {
+                return $"横:{right - left:0000}, 縦:{bottom - top:0000}  ({left}, {top}, {right}, {bottom})";
+            }
+        }
+        //座標取得用
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+
+        //手前にあるウィンドウのハンドル取得
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        //ウィンドウのRect取得
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        //ウィンドウのクライアント領域のRect取得
+        [DllImport("user32.dll")]
+        private static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        //クライアント領域の座標を画面全体での座標に変換
+        [DllImport("user32.dll")]
+        private static extern bool ClientToScreen(IntPtr hWnd, out POINT lpPoint);
+
+        //DWM（Desktop Window Manager）
+        //見た目通りのRectを取得できる、引数のdwAttributeにDWMWA_EXTENDED_FRAME_BOUNDSを渡す
+        //引数のcbAttributeにはRECTのサイズ、Marshal.SizeOf(typeof(RECT))これを渡す
+        //戻り値が0なら成功、0以外ならエラー値
+        [DllImport("dwmapi.dll")]
+        private static extern long DwmGetWindowAttribute(IntPtr hWnd, DWMWINDOWATTRIBUTE dwAttribute, out RECT rect, int cbAttribute);
+        
+        //ウィンドウ属性
+        //列挙値の開始は0だとずれていたので1からにした
+        enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_NCRENDERING_ENABLED = 1,
+            DWMWA_NCRENDERING_POLICY,
+            DWMWA_TRANSITIONS_FORCEDISABLED,
+            DWMWA_ALLOW_NCPAINT,
+            DWMWA_CAPTION_BUTTON_BOUNDS,
+            DWMWA_NONCLIENT_RTL_LAYOUT,
+            DWMWA_FORCE_ICONIC_REPRESENTATION,
+            DWMWA_FLIP3D_POLICY,
+            DWMWA_EXTENDED_FRAME_BOUNDS,//ウィンドウのRect
+            DWMWA_HAS_ICONIC_BITMAP,
+            DWMWA_DISALLOW_PEEK,
+            DWMWA_EXCLUDED_FROM_PEEK,
+            DWMWA_CLOAK,
+            DWMWA_CLOAKED,
+            DWMWA_FREEZE_REPRESENTATION,
+            DWMWA_LAST
+        };
+
+
+
+        //DC取得
+        //nullを渡すと画面全体のDCを取得、ウィンドウハンドルを渡すとそのウィンドウのクライアント領域DC
+        //失敗した場合の戻り値はnull
+        //使い終わったらReleaseDC
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        //渡したDCに互換性のあるDC作成
+        //失敗した場合の戻り値はnull
+        //使い終わったらDeleteDC
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        //指定されたDCに関連付けられているデバイスと互換性のあるビットマップを作成
+        //使い終わったらDeleteObject
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int cx, int cy);
+
+        //DCにオブジェクトを指定する、オブジェクトの種類はbitmap、brush、font、pen、Regionなど
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt(IntPtr hdc, int x, int y, int cx, int cy, IntPtr hdcSrc, int x1, int y1, uint rop);
+        private const int SRCCOPY = 0x00cc0020;
+        private const int SRCINVERT = 0x00660046;
+
+
+
+        [DllImport("user32.dll")]
+        private static extern bool DeleteDC(IntPtr hdc);
+
+        [DllImport("user32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr ho);
+
+
+
+
+        //ウィンドウ系のAPI
+        //Windows（Windowsおよびメッセージ）-Win32アプリ | Microsoft Docs
+        // https://docs.microsoft.com/en-us/windows/win32/winmsg/windows
+
+
+        #endregion コピペ呪文ここまで^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+        //タイマー用
+        DispatcherTimer MyTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //タイマー初期化
+            MyTimer = new DispatcherTimer();
+            MyTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);//0.1秒間隔
+            MyTimer.Tick += MyTimer_Tick;
+            MyTimer.Start();
+
+            //アプリ終了時、タイマーストップ
+            this.Closing += (s, e) => { MyTimer.Stop(); };
+        }
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            //キー入力取得用
+            //Keyを仮想キーコードに変換
+            int vKey1 = KeyInterop.VirtualKeyFromKey(Key.RightCtrl);
+            int vKey2 = KeyInterop.VirtualKeyFromKey(Key.RightShift);
+            //キーの状態を取得
+            short key1state = GetAsyncKeyState(vKey1);
+            short key2state = GetAsyncKeyState(vKey2);
+
+            //右Ctrlキー＋右Shiftキーが押されていたら
+            if ((key1state & 0x8000) >> 15 == 1 & ((key2state & 1) == 1))
+            {
+                //一番手前のウィンドウのハンドル取得
+                IntPtr hWindowForeground = GetForegroundWindow();                                                                 
+
+                //ウィンドウキャプチャ
+                Capture(hWindowForeground);
+            }
+        }
+
+        //ウィンドウキャプチャ
+        //画面全体をキャプチャして、そこからウィンドウのRect領域を切り抜いて表示
+        private void Capture(IntPtr hWnd)
+        {
+            //通常のRECT、見た目とのズレが有る
+            GetWindowRect(hWnd, out RECT windowRect);
+            //見た目通りのRect
+            DwmGetWindowAttribute(hWnd,
+                                  DWMWINDOWATTRIBUTE.DWMWA_EXTENDED_FRAME_BOUNDS,
+                                  out RECT windowExRect,
+                                  Marshal.SizeOf(typeof(RECT)));
+            //クライアント領域のRECT
+            GetClientRect(hWnd, out RECT clientRect);
+
+            MyTextBlock1.Text = $"{windowRect} 通常のWindowRect";
+            MyTextBlock2.Text = $"{windowExRect} 通常のWindowRectのズレ修正";
+            MyTextBlock3.Text = $"{clientRect} クライアント領域のRect";
+
+            int width = 1, height = 1;//切り抜きサイズ
+            int offsetX = 0, offsetY = 0;//切り抜きの左上座標
+            if (rbRect.IsChecked == true)
+            {
+                //通常RECT
+                width = windowRect.right - windowRect.left;
+                height = windowRect.bottom - windowRect.top;
+                offsetX = windowRect.left;
+                offsetY = windowRect.top;
+            }
+            else if (rbRectDwm.IsChecked == true)
+            {
+                //見た目通りのRect
+                width = windowExRect.right - windowExRect.left;
+                height = windowExRect.bottom - windowExRect.top;
+                offsetX = windowExRect.left;
+                offsetY = windowExRect.top;
+            }
+            else if (rbRectClient.IsChecked == true)
+            {
+                //クライアント領域RECT
+                width = clientRect.right;//GetClientRectのRECTのtopとleftは、
+                height = clientRect.bottom;//常に0なので引き算は不必要
+                //画面全体におけるクライアント領域の左上座標を取得
+                ClientToScreen(hWnd, out POINT cPoint);
+                offsetX = cPoint.X;
+                offsetY = cPoint.Y;
+            }
+
+            var screenDC = GetDC(IntPtr.Zero);//画面全体のDC、コピー元
+            var memDC = CreateCompatibleDC(screenDC);//コピー先DC作成
+            var hBmp = CreateCompatibleBitmap(screenDC, width, height);//コピー先のbitmapオブジェクト作成
+            SelectObject(memDC, hBmp);//コピー先DCにbitmapオブジェクトを指定
+
+            //コピー元からコピー先へビットブロック転送
+            //通常のコピーなのでSRCCOPYを指定
+            BitBlt(memDC, 0, 0, width, height, screenDC, offsetX, offsetY, SRCCOPY);
+            //bitmapオブジェクトからbitmapSource作成
+            BitmapSource source = 
+                Imaging.CreateBitmapSourceFromHBitmap(hBmp,
+                                                      IntPtr.Zero,
+                                                      Int32Rect.Empty,
+                                                      BitmapSizeOptions.FromEmptyOptions());
+            //後片付け
+            DeleteObject(hBmp);
+            ReleaseDC(IntPtr.Zero, screenDC);
+            ReleaseDC(IntPtr.Zero, memDC);
+
+            //画像表示
+            MyImage.Source = source;
+
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyImage.Source = null;
+            MyTextBlock1.Text = "";
+            MyTextBlock2.Text = string.Empty;
+            MyTextBlock3.Text = null;
+        }
+    }
+}
